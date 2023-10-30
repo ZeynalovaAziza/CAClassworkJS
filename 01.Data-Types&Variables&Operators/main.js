f31d6e749eae52c64c975fc9057b85c9af908f04
ZeynalovaAziza <zeynalofha2004@gmail.com>
2023-10-30 08:54:01 -0700

    01.dataJS

---
+++ 01.Data-Types&Variables&Operators/main.js
@@ -0,0 +1,45 @@
+let firstName = "Aziza";
+let lastName = "Zeynalova";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+let country = " Poland";
+let age = 20;
+let job = "Developer";
+let personInfo =
+  firstName +
+  " " +
+  lastName +
+  ".I live in" +
+  country +
+  ".I am " +
+  age +
+  " years old" +
+  ".I am a " +
+  job;
+console.log(personInfo);
+let length = 16; //number
+console.log(typeof length);
+let color = "brown"; //string
+console.log(typeof color);
+
+let float = "12.5"; //string
+let like = true; // booleun
+let x = { firstname: "Nick", color: "Red" }; //object
+console.log(typeof x);
+let distance = 8600;
+let speed = 100;
+let duration = distance / speed;
+
+console.log("The car will arrive in China in " + duration + " minutes");
+
+let birthYear = 2004;
+let thisYear = 2023;
+let Age = birthYear - thisYear;
+
+console.log("I will be either " + age + " or " + (age + 1));
+let aziza = 100;
+let nargis = 95;
+let sabina = 95;
+let qanira = 95;
+let ortalama=(aziza+nargis+sabina+qanira)/4;
+console.log(ortalama);

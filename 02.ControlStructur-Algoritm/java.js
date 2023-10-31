0e43174deb7b795d401347a872f07b5112a02329
ZeynalovaAziza <zeynalofha2004@gmail.com>
2023-10-31 08:50:14 -0700

    02.JS

---
+++ 02.ControlStructur-Algoritm/java.js
@@ -0,0 +1,74 @@
+//    Task1
+let degree=13
+if(degree>22){
+    console.log("nazik geyin");
+}else if(degree>15){
+    console.log("pencek geyin");
+}else{
+   console.log("qalin geyin");
+}
+
+//   Task2
+let letter="d"
+if (letter=="a"|| letter=="i"|| letter=="u"||letter=="o"||letter=="e"){
+    console.log("saitdir");
+}else{
+    console.log("samitdir");
+}
+
+// Task3
+// let a = 13;
+// let b = 111;
+// let c = 777;
+// if (a > b && a > c) {
+//   console.log(`${a} is greater`);
+// } else if (b > a && b > c) {
+//   console.log(`${b} is greater`);
+// } else {
+//   console.log(`${c} is greater`);
+// }
+
+//    Task4
+let score = 91;
+if (score > 90) {
+  console.log("exam grade:A");
+} else if (score > 80) {
+  console.log("exam grade:B");
+} else if (score > 70) {
+  console.log("exam grade:C");
+} else if (score > 60) {
+  console.log("exam grade:D");
+}else if(score>50){
+ console.log("exam grade:F");
+}else{
+    console.log("fail");
+}
+
+    // Task5
+    let firstName="Aziza"
+    if(firstName.length<=5){
+        console.log(`salam ${firstName}.Qisa adiniz var`);
+    }else if(firstName.length<10){
+        console.log(`salam ${firstName}.Orta uzunluqlu adiniz var`);
+    }else {
+        console.log(`salam ${firstName}.Adiniz uzundur`);
+    }
+
+
+    // Task6
+    // let a=1;
+    // let b=2;
+    // let c=3;
+    // if(a+b>a+c && a+b>b+c){
+    //     console.log(`${a} + ${b} = ${a+b} is biggest`);
+    // }else if(a+c>a+b && a+c>b+c){
+    //     console.log(`${a} + ${c} = ${a+c} is biggest`);
+    // }else {
+    //     console.log(`${b} + ${c} = ${b+c} is biggest`);
+    // }
+
+   
+    //  Task7
+    let a=4;
+    let b=5;
+    let c=6;

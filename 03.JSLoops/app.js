eb40082694a6082349d1a4dd985c32e2c47b5438
ZeynalovaAziza <zeynalofha2004@gmail.com>
2023-11-01 08:43:59 -0700

    03.JSloops

---
+++ 03.JSLoops/app.js
@@ -0,0 +1,61 @@
+// task1
+
+// for(i=1;i<100;i++){
+//     if(i%2==0)
+//     console.log(i);
+// }
+
+// task2
+
+// let str="Aziza";
+// for(i=0;i<str.length;i++){
+//     console.log(str[i]);
+// }
+//  task3
+
+// for(i=10;i<100;i++){
+//     if(i%10===7)
+//     console.log(i);
+// }
+
+// task4
+
+// for(i=11;i<100;i++){
+//     if(i%11==0)
+//     console.log(i);
+// }
+
+// task5
+// 1,2,3,6,9,18
+// let n=18
+// for(i=1;i<=18;i++){
+//     if(n%i==0)
+//     console.log(i);
+// }
+
+// task7
+// let sum=0;
+// for(i=1;i<10;i++){
+//     if(i%2==0){
+//         sum+=i
+//     }
+// }
+// console.log(sum)
+
+// task8
+// let sum=0;
+// for(i=10;i<50;i++){
+//     if(i%3==0){
+//         sum+=i
+//     }
+// }
+// console.log(sum);
+
+// task6
+// let n = 4;
+// let factorial = 1;
+// for (i = 1; i <= n; i++) {
+//   factorial *= i;
+// }
+// console.log(factorial);
+

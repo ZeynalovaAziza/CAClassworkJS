2886e55070fe48da3c5924c72448e5c639b07ff8
ZeynalovaAziza <zeynalofha2004@gmail.com>
2023-11-03 09:04:26 -0700

    JS.array-04

---
+++ 04.JS.Array&Object/app.js
@@ -0,0 +1,35 @@
+// task1
+// let numbers=[12,45,6,78,34,99,23];
+// let max=numbers[0];
+// let min=numbers[0];
+// for(let i=1;i<numbers.length;i++){
+//     if(numbers[i]>max){
+//         max=numbers[i]
+//     }else if(numbers[i]<min) {
+//         min=numbers[i]
+//     }
+// }console.log("MAX: ",max);
+// console.log("MIN: ",min);
+
+// task2
+// let numbers=[12,45,6,78,34,99,23];
+// let sum=0;
+// for(let i=0;i<numbers.length;i++){
+//     sum+=numbers[i]
+// }console.log(sum);
+
+// task3
+// let elements=[1,2,3,4,5];
+// temp=elements[2];
+// elements[2]=elements[3];
+// elements[3]=temp
+// console.log(elements);
+
+// task4
+let numbers=[12,-5,6,-3,34,-1,23];
+let positive=[];
+if(numbers[i]>0){
+    push(positive[i])
+}console.log();
+
+

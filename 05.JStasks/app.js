9093005ff2ea595786cf037d26d8e7b3c7f8f3fb
ZeynalovaAziza <zeynalofha2004@gmail.com>
2023-11-04 06:43:42 -0700

    05.JStask

---
+++ 05.JStasks/app.js
@@ -0,0 +1,32 @@
+// task1
+// let array= new Array()
+
+
+// task2
+let itCompanies=["Facebook","Google","Microsoft","Apple","IBM","Oracle","Amazon"]
+
+// task3
+// console.log(itCompanies.length);
+
+// task4
+// for(let i=0;i<itCompanies.length;i++){
+//     console.log(itCompanies[i]);
+// }
+
+// task5
+// console.log(itCompanies[0]);
+// console.log(itCompanies[itCompanies.length-1]);
+// console.log(itCompanies[parseInt(itCompanies.length/2)]);
+
+// task6
+// let newCompanies=[]
+// for (let i= 0; i < itCompanies.length; i++) {
+//     itCompanies[i].toLocaleUpperCase()
+//     newCompanies.push(itCompanies[i].toLocaleUpperCase())
+// }
+// console.log(newCompanies);
+
+// task7
+console.log(itCompanies.toString());
+
+

7a72ac5c1f8273bdea69160b5156fa97f052fa69
ZeynalovaAziza <zeynalofha2004@gmail.com>
2023-11-17 07:46:59 -0800

    12.JS-practice

---
+++ 12.JS-Practice/app.js
@@ -0,0 +1,17 @@
+// task 1
+// function reverse(num) {
+//     let result= String(num).split("").reverse().join("");
+  
+//     return result
+//   }
+//   console.log(reverse(32243));
+
+
+// task 2
+let str='webmaster'
+console.log(str.split("").sort().join(""));
+
+
+// task3
+
+let string = 'the quick brown fox'

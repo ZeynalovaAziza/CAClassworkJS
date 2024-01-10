23afad801a2147e0d329cdce446e9de330ef3f8b
ZeynalovaAziza <zeynalofha2004@gmail.com>
2024-01-10 11:21:58 +0400

    furniture

--- 32-JS.FurnishWebsite.exam/assets/js/app.js
+++ 32-JS.FurnishWebsite.exam/assets/js/app.js
@@ -1,4 +1,5 @@
 let boxes = document.querySelector(".boxes")
+let search=document.querySelector(".search")
 const BASE_URL ="http://localhost:8080"
 
 async function getData(endpoint){
@@ -18,8 +19,31 @@ function drawBox(data) {
       <img src="${element.photo}" alt="" />
       <h4>${element.title}</h4>
       <p>${element.description}</p>
+      <button class="deleteBtn"onclick=deleteFurniture("${element.id}",this)>DELETE</button>
     </div>
       
       `;
     });
   }
+
+  function deleteCustomer(id, btn) {
+    if (confirm("Are u sure to delete?")) {
+      fetch(`${BASE_URL}/furniture/${id}`, {
+        method: "DELETE",
+      });
+  
+      btn.closest(".box").remove();
+    }
+  }
+
+  search.addEventListener("input", function (event) {
+    fetch(`${BASE_URL}/furniture${event.target.value.toLocaleLowerCase()}`)
+    .then((response)=>response.json())
+      .then((data) => {
+        drawBox(data);
+      })
+
+  });
+  
+  
+  
